2d2874b380d0aac4b882bf4616e314d5b5f4f0a8
Vlad Balanov <vladislav.balanov@gmail.com>
2020-08-27 20:55:38 +0300

    added log appearing to changeHP function

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const $btnKick = document.getElementById(`btn-kick`);
 const $btnWhip = document.getElementById(`btn-whip`);
 
+const $logs = document.querySelector('#logs');
+
+
 const character = {
   name: `Pikachu`,
   defaultHP: 300,
@@ -62,8 +65,12 @@ function renderHP() {
 function changeHP(count) {
   this.damageHP -= count;
 
-  const log = this === enemy ? generateLog(this, character) : generateLog(this, enemy);
+  const log = this === enemy ? generateLog(this, character, count) : generateLog(this, enemy, count);
   console.log(log);
+
+  const $p = document.createElement('p');
+  $p.innerText = log;
+  $logs.insertBefore($p, $logs.children[0]);
 
   if (this.damageHP <= count) {
     this.damageHP = 0;
@@ -78,23 +85,23 @@ function random(num) {
   return Math.ceil(Math.random() * num);
 }
 
-function generateLog(firstPerson, secondPerson) {
-  const {name: name1, defaultHP, damageHP, changeHP: dmg} = firstPerson;
+function generateLog(firstPerson, secondPerson, count) {
+  const {name: name1, defaultHP, damageHP} = firstPerson;
   const {name: name2} = secondPerson;
 
   const logs = [
-    `${name1} вспомнил что-то важное, но неожиданно ${name2}, не помня себя от испуга, ударил в предплечье врага. [${damageHP}/${defaultHP}]`,
-    `${name1} поперхнулся, и за это ${name2} с испугу приложил прямой удар коленом в лоб врага. [${damageHP}/${defaultHP}]`,
-    `${name1} забылся, но в это время наглый ${name2}, приняв волевое решение, неслышно подойдя сзади, ударил. [${damageHP}/${defaultHP}]`,
-    `${name1} пришел в себя, но неожиданно ${name2} случайно нанес мощнейший удар. [${damageHP}/${defaultHP}]`,
-    `${name1} поперхнулся, но в это время ${name2} нехотя раздробил кулаком \<вырезанно цензурой\> противника. [${damageHP}/${defaultHP}]`,
-    `${name1} удивился, а ${name2}, пошатнувшись, влепил подлый удар. [${damageHP}/${defaultHP}]`,
-    `${name1} высморкался, но неожиданно ${name2} провел дробящий удар. [${damageHP}/${defaultHP}]`,
-    `${name1} пошатнулся, и внезапно наглый ${name2} беспричинно ударил в ногу противника. [${damageHP}/${defaultHP}]`,
-    `${name1} расстроился, как вдруг, неожиданно ${name2} случайно влепил стопой в живот соперника. [${damageHP}/${defaultHP}]`,
-    `${name1} пытался что-то сказать, но вдруг, неожиданно ${name2} со скуки, разбил бровь сопернику. [${damageHP}/${defaultHP}]`,
+    `${name1} вспомнил что-то важное, но неожиданно ${name2}, не помня себя от испуга, ударил в предплечье врага.`,
+    `${name1} поперхнулся, и за это ${name2} с испугу приложил прямой удар коленом в лоб врага.`,
+    `${name1} забылся, но в это время наглый ${name2}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
+    `${name1} пришел в себя, но неожиданно ${name2} случайно нанес мощнейший удар.`,
+    `${name1} поперхнулся, но в это время ${name2} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
+    `${name1} удивился, а ${name2}, пошатнувшись, влепил подлый удар.`,
+    `${name1} высморкался, но неожиданно ${name2} провел дробящий удар.`,
+    `${name1} пошатнулся, и внезапно наглый ${name2} беспричинно ударил в ногу противника.`,
+    `${name1} расстроился, как вдруг, неожиданно ${name2} случайно влепил стопой в живот соперника.`,
+    `${name1} пытался что-то сказать, но вдруг, неожиданно ${name2} со скуки разбил бровь сопернику.`,
   ];
-  return logs[random(logs.length - 1)];
+  return `${logs[random(logs.length - 1)]} -${count} [${damageHP}/${defaultHP}]`;
 }
 
 init();
